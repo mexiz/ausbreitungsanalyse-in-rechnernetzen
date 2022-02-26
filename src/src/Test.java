7c8ab9bef194db21e69160b9241617b5317ca458
Markus Neufeld <uwhlp@student.kit.edu>
2022-02-26 13:50:15 +0100

    Describing_TupleParserclass

--- src/src/Test.java
+++ src/src/Test.java
@@ -5,8 +5,11 @@ public class Test {
 
     public static void main(String[] args) throws ParseException {
 
-        new TupleParser().getTree("(85.193.148.81 (141.255.1.133 122.117.67.158 0.146.197.108) 34.49.145.239 (231.189.0.127 77.135.84.171 39.20.222.120 252.29.23.0 116.132.83.77))");
+        List<Tuple<IP>> test = new TupleParser().getTree("(85.193.148.81 (141.255.1.133 122.117.67.158 0.146.197.108) 34.49.145.239 (231.189.0.127 77.135.84.171 39.20.222.120 252.29.23.0 116.132.83.77))");
 
+        for (Tuple<IP> tuple : test) {
+            System.out.println(tuple.connection());
+        }
     }
 
 }
--- src/src/TupleParser.java
+++ src/src/TupleParser.java
@@ -2,43 +2,68 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Die Parser für die Umformung von der Bracketnotation in die Tupel Notation und umgekehrt.
+ * 
+ * @author uwlhp
+ * @version 1.0
+ */
+
 public class TupleParser {
 
     private String regexByte = "(((2[0-5][0-5])|(2[0-4]\\d)|(1\\d\\d)|([1-9]\\d)|([1-9])|(0)))";
     private String regexIP = "\\b(" + regexByte + "\\.){3}" + regexByte + "\\b";
+    private static final String SEPARATOR_IP = " ";
 
-
+    /**
+     * Die Methode wandelt den Baum, der als Bracketnotation dargestellt wird,
+     * in die Tupelnotation um.
+     * 
+     * @param bracketNotation Der Baum als Bracketnotation
+     * @return Gibt eine List von Tupeln zurück
+     * @throws ParseException Wenn die Bracketnotation- oder die Adressenform nicht richtig ist
+     */
+    
     public List<Tuple<IP>> getTree(final String bracketNotation) throws ParseException {
-
+        //Entfernt die äußerste Klammer
         String bracket = bracketNotation.substring(1, bracketNotation.length() - 1);
-        List<String> splitted = new ArrayList<>(Arrays.asList(bracket.split(" ")));
+        List<String> splitted = new ArrayList<>(Arrays.asList(bracket.split(SEPARATOR_IP)));
         List<Tuple<IP>> table = new ArrayList<>();
         String root = splitted.get(0);
         int i = 1;
-        
+
+        // Solange bis die Liste splitted nur noch den Root hat
         while (i < splitted.size()) {
             String ip = splitted.get(i);
             if (ip.charAt(0) == '(') {
                 table.add(new Tuple<>(new IP(root), new IP(ip.substring(1))));
                 int end = this.indexBracketClosed(splitted, i);
+
+                // Rekursion um eine Ebene tiefer in den Graphen zu kommen
                 List<Tuple<IP>> supTuples = this.getTree(this.toString(splitted, i, end));
                 table.addAll(supTuples);
+
+                // Löscht die Subliste
                 splitted.subList(i, end).clear();
             } else if (ip.charAt(ip.length() - 1) == ')') {
-                if (ip.charAt(ip.length() - 2) != ')') {
-                    ip = ip.substring(0, ip.length() - 1);
-                    table.add(new Tuple<>(new IP(root), new IP(ip)));
-                    
-                    splitted.remove(i);
-                }
+                ip = ip.substring(0, ip.length() - 1);
+                table.add(new Tuple<>(new IP(root), new IP(ip)));
+                splitted.remove(i);
             } else {
-
                 table.add(new Tuple<>(new IP(root), new IP(ip)));
                 splitted.remove(i);
             }
         }
         return table;
     }
+
+    /**
+     * Die Hilfsmethode gibt den Index zurück, wo sich der Substrang vom Baum
+     * schließt.
+     * @param list Die Liste mit den Addressen von der Bracketnotation  
+     * @param start Der Startwert, wo der Substrang begonnen hat
+     * @return git den Index zurück
+     */
 
     private int indexBracketClosed(List<String> list, int start) {
 
@@ -62,6 +87,14 @@ public class TupleParser {
         return i;
     }
 
+    /**
+     * Die Hilfsmethode verbindet die durch den {@link TupleParser#SEPARATOR_IP}  getrennte Liste zusammen.
+     * @param list Die durch ein {@link TupleParser#SEPARATOR_IP} getrennte Liste 
+     * @param start Startwert der Liste
+     * @param end Endwert der Liste
+     * @return Gibt den zusammengesetzten String zurück
+     */
+
     private String toString(List<String> list, int start, int end) {
 
         List<String> l = list.subList(start, end);
@@ -70,7 +103,7 @@ public class TupleParser {
 
         for (int i = 0; i < l.size() - 1; i++) {
             sb.append(l.get(i));
-            sb.append(" ");
+            sb.append(SEPARATOR_IP);
         }
 
         sb.append(l.get(l.size() - 1));
