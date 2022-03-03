8b69cb05ef260522fb6b3313fbe95622bc36b688
Markus Neufeld <uwhlp@student.kit.edu>
2022-03-03 19:09:48 +0100

    changes

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,8 @@
 public class Test {
 
     public static void main(String[] args) throws ParseException {
-        
+        Network net = new Network("(1.0.0.0 2.0.0.0 3.0.0.0)");
+        net.getHeight(new IP("1.0.0.0"));
     }
 
 }
