5b1d5f241045e5863de32dce0615912379a03096
Markus Neufeld <uwhlp@student.kit.edu>
2022-03-04 10:20:48 +0100

    fixed checkstyle

---
+++ src/edu/kit/informatik/Test.java
@@ -0,0 +1,26 @@
+package edu.kit.informatik;
+
+
+/**
+ * 
+ * Testklasse
+ * 
+ * @author uwhlp
+ * @version 1.0
+ * 
+ */
+
+public class Test {
+
+    /**
+     * Main-Methde
+     * 
+     * @param args args
+     * @throws ParseException Exception
+     */
+    public static void main(String[] args) {
+
+        System.out.println("ENDE");
+    }
+
+}
