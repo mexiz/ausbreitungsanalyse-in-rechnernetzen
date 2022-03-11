38f148cf7f72342266fd6ed8e57687ba322d2a73
Markus Neufeld <uwhlp@student.kit.edu>
2022-03-11 16:54:08 +0100

    smaller

--- src/edu/kit/informatik/Network.java
+++ src/edu/kit/informatik/Network.java
@@ -200,10 +200,7 @@ public class Network {
         if (this == obj) {
             return true;
         }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
+        if (obj == null || getClass() != obj.getClass()) {
             return false;
         }
 
